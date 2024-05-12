b9f9fdb74dddbfbeebef42b1f8aa9579e60a9783
Brian <thinhle1199@gmail.com>
2024-05-12 14:23:17 -0500

    first commit

---
+++ src/main/java/com/brian/springsecuritywithjsonwebtoken/SpringSecurityWithJsonWebTokenApplication.java
@@ -0,0 +1,13 @@
+package com.brian.springsecuritywithjsonwebtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJsonWebTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithJsonWebTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brian/springsecuritywithjsonwebtoken/SpringSecurityWithJsonWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.springsecuritywithjsonwebtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithJsonWebTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
